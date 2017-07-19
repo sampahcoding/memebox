0bbacc2e1210e85f201bf364b1b90fe4b8cc05cd
ana syarifah <anasyarifah@MacBook-Air.local>
2017-07-20 01:53:47 +0700

    Add gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+
 gulp.task('sass', function () {
   gulp.src('./app/styles/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('all.css'))
     .pipe(gulp.dest('./styles'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./app/styles/**/*.scss', ['sass']);
 });
