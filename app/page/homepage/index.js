c336dee997a9a0a7a7ccb3a37e452b356543e56b
ana syarifah <anasyarifah@MacBook-Air.local>
2017-07-21 16:06:33 +0700

    install bootstrap for admin application

--- app/page/homepage/index.js
+++ app/page/homepage/index.js
@@ -2,6 +2,8 @@ import Backbone from 'backbone';
 import $ from 'jquery';
 import Router from '../../routers/router';
 var DashboardApp = require("../dashboard/index").default;
+// add this to admin application
+//require('bootstrap/dist/css/bootstrap.css');
 
 $(() => {
   new Router();
